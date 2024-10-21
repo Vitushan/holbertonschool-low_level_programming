192f90e4d59bb28445b3f6799f1a1e2ea1e585c3
vitushan <vitushan@outlook.fr>
2024-10-21 15:09:46 +0000

    re

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("n:%d", n);
-	if (n > 0)
+	printf("n: %d", n);
+	if (n > 0){
 		printf("is positive\n");
-	else if (n == 0)
+	}else if (n == 0){
 		printf("is zero\n");
-	else
+	}else{
 		printf("is negative\n");
-	 
+	}
 	return (0);
 }
