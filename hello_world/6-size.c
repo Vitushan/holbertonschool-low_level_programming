cbea944695fb24d813c4d0ad9664c3ab49c64688
vitushan <vitushan@outlook.fr>
2024-10-21 13:14:31 +0000

    e

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %ld",sizeof (char));
 	printf("Size of a int: %ld",sizeof (int));
-	printf("Size of a long int: %ld",sizeof (long);
+	printf("Size of a long int: %ld",sizeof (long));
 	printf("Size of a long long int: %ld",sizeof (long));
-	printf("Size of a float: %d",sizeof (float);
+	printf("Size of a float: %d",sizeof (float));
 	return (0);
 }
