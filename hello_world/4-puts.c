73c751a98d5847d016f182e3a061a5b0afc04d8e
vitushan <vitushan@outlook.fr>
2024-10-21 10:06:50 +0000

    push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-int main(void)
+#include <stdio.h> //bibliotheque
+int main(void) // main principal
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");//affiche message a l'ecran
+  return (0); //renvoie 0 si compiler avec succés
 }
