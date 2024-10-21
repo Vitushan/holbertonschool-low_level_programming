5dab3f6708255f05012446aa802ce575ce3818fb
vitushan <vitushan@outlook.fr>
2024-10-21 10:10:25 +0000

    oui

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include <stdio.h> //bibliotheque
-int main(void) // main principal
+#include <stdio.h>
+
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");//affiche message a l'ecran
-  return (0); //renvoie 0 si compiler avec succés
+  return (0);
 }
