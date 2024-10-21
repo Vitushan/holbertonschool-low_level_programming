b7c4980716da7925277df933ed0fad144260777e
vitushan <vitushan@outlook.fr>
2024-10-21 11:14:46 +0000

    oui

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /* main is function for print*/
 int main(void)/*function main*/
-{ /*function main*/
+{
 puts("\"Programming is like building a multilingual puzzle");/*main function is main function*/
 return (0);
 }
