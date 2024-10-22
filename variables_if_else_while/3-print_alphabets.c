16dee6c314912d186d8bf299c3391f5800fa898a
vitushan <vitushan@outlook.fr>
2024-10-22 15:45:30 +0000

    else

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,13 @@ int main(void)
 char min = 'a', maj = 'A';
 
 for (min = 'a'; min <= 'z'; min++)
-  {
+	{
 	putchar(min);
-  }	
+	}	
 for (maj = 'A'; maj <= 'Z'; maj++)
-  {
+	{
 	putchar(maj);
-  }
+	}
 	putchar('\n');
 	return (0);
 }
