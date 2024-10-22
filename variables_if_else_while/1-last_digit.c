894df37348102ab167020e47a23c9d05e3755a1a
vitushan <vitushan@outlook.fr>
2024-10-22 11:40:37 +0000

    else

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 int n, last_digit;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
@@ -27,6 +26,5 @@ int n, last_digit;
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
-
-        return (0);
+	return (0);
 }
