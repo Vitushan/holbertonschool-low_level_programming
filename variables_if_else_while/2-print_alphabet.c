8633b65c95896af8134c7393164106218ecb16f5
vitushan <vitushan@outlook.fr>
2024-10-22 14:07:17 +0000

    loop

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main (void)
 {
-  char alpha = 'a';
+	char alpha = 'a';
   
-  for (alpha ='a'; alpha <= 'z'; alpha++)
-    {
-      putchar(alpha);
-    }
-  putchar('\n');
-  return 0;
+	for (alpha ='a'; alpha <= 'z'; alpha++)
+	{
+	putchar(alpha);
+	}
+	putchar('\n');
+	return 0;
 }
