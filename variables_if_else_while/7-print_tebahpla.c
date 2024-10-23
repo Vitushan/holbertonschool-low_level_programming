32940a5694db4cecf7e54e1123d9d25edda703e4
vitushan <vitushan@outlook.fr>
2024-10-23 09:40:16 +0000

    d

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+ /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+        int i = 0;
+	
+	for (alpha ='a'; alpha >= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+		return (0);
+}
