6239643d7df14f4bdd34960a727f750cd6f935ba
vitushan <vitushan@outlook.fr>
2024-10-23 14:01:55 +0000

    loop

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int nb1 = 0;
-	
-	for (nb1 = 48; nb1 <= 57; nb1++) 
-       {
+
+	for (nb1 = 48; nb1 <= 57; nb1++)
+	{
 		putchar(nb1);
-       
+
 		if (nb1 != 57)
 		{
 			putchar(',');
 			putchar(' ');
-		}	
-       }
+		}
+	}
 	putchar('\n');
 	return (0);
 }
