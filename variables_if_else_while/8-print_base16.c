e66ceaf7c79c649c119378f3b7c32212b806726b
vitushan <vitushan@outlook.fr>
2024-10-23 12:39:07 +0000

    loops

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,12 +7,17 @@
 */
 int main(void)
 {
-	int hexa = 15;
-		
-		for(hexa = 0; hexa <= 15; hexa++)
+	int hexa = 0;
+	
+	for(hexa = 48; hexa <= 57; hexa++) 
 		{
-			putchar(hexa);
+			putchar(hexa + 0);
 		}
+	for (hexa = 97; hexa <= 102; hexa++)
+		{
+			putchar(hexa+ 0);
+		}
+	
 	putchar('\n');
 	return (0);
 }
