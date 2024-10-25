cff4a488c223060959e02ecb62f389288ee826f6
vitushan <vitushan@outlook.fr>
2024-10-25 08:47:28 +0000

    re

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
-int main(void)
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
 {
-	_putcharvoid print_alphabet(void);
-	return (0);
-}
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+		_putchar('\n');
+		return (0);
