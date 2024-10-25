9275bfb15340e245625b127ebd0f41fe83c9bf3c
vitushan <vitushan@outlook.fr>
2024-10-25 11:36:15 +0000

    for

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	if ( 'c' == 'c')
+	_putchar(alpha);
