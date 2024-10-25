5ddd386d16744b289f572a811822cecafc771550
vitushan <vitushan@outlook.fr>
2024-10-25 13:10:43 +0000

    e

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- *
- *_islower(int c) - code return 0 or 1
- *
+ * _islower - code return 0 or 1
+ * @c: the character to check
  * Return: Always 0.
  */
+
 int _islower(int c)
 {
 	if ('a' <= c && c <= 'z')
