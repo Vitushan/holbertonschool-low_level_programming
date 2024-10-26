42957a45ed1247adeb1ab6a1884670e4b84e7d8e
vitushan <vitushan@outlook.fr>
2024-10-26 18:21:53 +0000

    push

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * -sign - Compare if a number is great less or equal of 0
- *@n: Integer to check, given an ascii code.
- *Return 1 and prints + if n is superior a zero
- *Return 0 and prints 0 if n is zero
- *Return -1 and prints less a  zero
+ * -sign - 
+ * Compare if a number is great less or equal of 0
+ *@n: Integer type
  */
 int print_sign(int n)
 {
