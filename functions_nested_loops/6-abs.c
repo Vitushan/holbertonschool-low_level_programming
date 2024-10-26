506d39b2236cd83e18aa6e1a2eb1f27a5c21a982
vitushan <vitushan@outlook.fr>
2024-10-26 20:09:59 +0000

    push

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * int _abs(int n) - code return -n
+ * prototype: int _abs(int)
+ *calculate absolute functions
+ *@n: The integer type
+ */
 int _abs(int n)
 {
 	if (n < 0)
