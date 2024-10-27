af349dc7f772a00e37896277a5935cf484b8433f
vitushan <vitushan@outlook.fr>
2024-10-27 18:30:58 +0000

    integer

--- functions_nested_loops/11-main.c
+++ functions_nested_loops/11-main.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_to_98 - Prints all natural numbers from n to 98, separated by a comma and a space.
+ * @n: The starting number.
  */
-int main(void)
+void print_to_98(int n)
 {
-    print_to_98(0);
-    print_to_98(98);
-    print_to_98(111);
-    print_to_98(81);
-    print_to_98(-10);
-    return (0);
+    while (n != 98)
+    {
+        _putchar(n + '0'); // Print the current number
+        if (n != 98)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
+        n += (n < 98) ? 1 : -1; // Increment or decrement n
+    }
+    _putchar('9'); // Print the tens digit of 98
+    _putchar('8'); // Print the units digit of 98
+    _putchar('\n'); // New line
 }
