6955b2d101524d76696ec96482cb87ce120a4fbc
vitushan <vitushan@outlook.fr>
2024-10-27 12:44:25 +0000

    re

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+ int table_id = 0, line = 0;
+
+ for (; table_id <= 9)
+
+	{
+		for (; line <= 0)
+	}
+
+	return (table_id * line);
+}
