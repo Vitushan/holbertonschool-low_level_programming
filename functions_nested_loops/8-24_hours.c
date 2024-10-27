fe48bde96d3a1c7f201f35c1a1df0090684145d7
vitushan <vitushan@outlook.fr>
2024-10-27 11:01:42 +0000

    re

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,12 +7,12 @@
  */
 void jack_bauer(void)
 {
-    int hours, minutes;
+	int hours, minutes;
 
 	for (hours = 0; hours < 24; hours++)
 	{
 		for (minutes = 0; minutes < 60; minutes++)
-		{	
+		{
 		_putchar((hours / 10) + '0');
 		_putchar((hours % 10) + '0');
 		_putchar(':');
