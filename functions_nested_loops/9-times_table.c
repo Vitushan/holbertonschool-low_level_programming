c9eee7ed49f27db6d47715e0faf7c70cee3b383e
vitushan <vitushan@outlook.fr>
2024-10-27 13:11:22 +0000

    re

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,8 +8,8 @@
 void times_table(void)
 {
 int table_id, line, product;
-	
-    		for (table_id = 0; table_id <= 9; table_id++)
+
+		for (table_id = 0; table_id <= 9; table_id++)
 		{
 		for (line = 0; line <= 9; line++)
 		{
@@ -17,25 +17,25 @@ int table_id, line, product;
 
 		if (line > 0)
 		{
-		_putchar(',');
-		_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 		}
 
 		if (product < 10 && line > 0)
 		{
-                _putchar(' ');
+			_putchar(' ');
 		}
 
 		if (product >= 10)
             	{	
-                _putchar((product / 10) + '0');
+			_putchar((product / 10) + '0');
             	}
-		_putchar((product % 10) + '0');
+			_putchar((product % 10) + '0');
 
 		if (line == 9)
 		{
                 _putchar('\n');
-		}		
+		}
 	}
 	}
 }
