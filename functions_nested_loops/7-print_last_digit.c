99e751d08899541334931f59452776454b41537a
vitushan <vitushan@outlook.fr>
2024-10-27 09:34:09 +0000

    re

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+}	
+
