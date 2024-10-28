17965013af65756fba0dd728f710aae15683ddaa
vitushan <vitushan@outlook.fr>
2024-10-28 12:45:20 +0000

    loops

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,5 +7,15 @@
  */
 void print_numbers(void)
 {
+	int number, nb2 = 2, nb4 = 4;
 
+	for (number = 0; number < 10; number++)
+	{
+	if (nb2 != 2 && ,nb4 != 4;
+	
+		_putchar(number)
+	}
+		_putchar('\n');
+	
+	return (0);
 }
