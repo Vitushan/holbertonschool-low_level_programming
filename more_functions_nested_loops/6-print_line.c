b70dd9d169255cc92da6177c3d8e2657f9e12482
vitushan <vitushan@outlook.fr>
2024-10-28 15:21:16 +0000

    r

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,25 @@
 #include "main."
+/**
+ * print_line - Draws a straight line in the terminal.
+ * @n: The number of times to print the character '_'.
+ *
+ * Return: Always void.
+ */
 
 void print_line(int n)
 {
-
-
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+			_putchar('\n');
+		}
 }
+
+
