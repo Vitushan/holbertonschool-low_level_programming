6b92a87dad87a6b44a5656676ccc4fa1691a9bcc
vitushan <vitushan@outlook.fr>
2024-10-28 10:05:46 +0000

    r

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,7 @@
 /**
 * _isdigit - Check if the character is a digit (0-9)
 * @c: The character to check
-* 
-* return 1 if c is a digit, 
+* return 1 if c is a digit,
 * return 0 for otherwise.
 */
 int _isdigit(int c)
