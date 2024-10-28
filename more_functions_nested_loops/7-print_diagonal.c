fc41a9c0a9db6fcd4a5be4f40bfabc979a27ea4c
vitushan <vitushan@outlook.fr>
2024-10-28 15:49:38 +0000

    r

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,25 @@
 #include "main.h"
+
 /**
  * print_line - Draws a straight line in the terminal.
  * @n: The number of times to print the character '_'.
  *
  * Return: Always void.
  */
-
-void print_line(int n)
+void print_diagonal(int n)
 {
-	if (n <= 0)
+
+	if ( n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
 		int i;
-
 		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 			_putchar('\n');
-		}
+	}
 }
