728741361282d2b11fdddc9e6e49f10a3602560c
vitushan <vitushan@outlook.fr>
2024-10-28 10:00:24 +0000

    r

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,8 @@
 /**
 * _isdigit - Check if c is number
 * followed by a new line.
-* @n: The checking number.
+* @n: The checking number
+* return 1 if number and 0 if not number
 */
 int _isdigit(int c)
 {
