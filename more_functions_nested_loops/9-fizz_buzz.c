d5177890bb38f3b04dc604cbad8a2a6b9b5ca047
vitushan <vitushan@outlook.fr>
2024-10-29 11:05:09 +0000

    for

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-	
-	for (int number = 1; number <= 100; number ++)
+	int number;
+
+	for (number = 1; number <= 100; number ++)
 	{
 	if (number % 3 == 0 && number % 5 == 0)
 	{
