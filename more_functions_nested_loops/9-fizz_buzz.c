634cbe3fc2e06e49d8f240aed7ca37ad6fad6144
vitushan <vitushan@outlook.fr>
2024-10-29 11:14:43 +0000

    for

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Implements the Fizz-Buzz test
+ *
+ * Description: Prints numbers from 1 to 100, but for multiples of three,
+ * it prints "Fizz" instead of the number, and for multiples of five,
+ * it prints "Buzz". For numbers which are multiples of both three and five,
+ * it prints "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int number;
