9744b812f5282337e700d19491ef5501e4285985
vitushan <vitushan@outlook.fr>
2024-10-29 13:06:55 +0000

    r

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,15 +16,15 @@ void print_triangle(int size)
 	{
 		int i;
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i < size; i++)
 		{
 			int j;
 
 			for (j = 0; j < i; j++)
 			{
-				_putchar(' ');
+				_putchar('#');
 			}
-			_putchar('#');
+			_putchar(' ');
 			_putchar('\n');
 		}
 	}
