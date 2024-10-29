680c4d647de241d060798be1d9f3627a7c99f7c2
vitushan <vitushan@outlook.fr>
2024-10-29 11:08:01 +0000

    for

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,17 +4,17 @@ int main(void)
 {
 	int number;
 
-	for (number = 1; number <= 100; number ++)
+	for (number = 1; number <= 100; number++)
 	{
 	if (number % 3 == 0 && number % 5 == 0)
 	{
 		printf("FizzBuzz");
 	}
 	else if (number % 3 == 0)
-	{	
+	{
 		printf("Fizz");
 	}
-	else if ( number % 5 == 0)
+	else if (number % 5 == 0)
 	{
 		printf("Buzz");
 	}
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("%d", number);
 	}
-	
+
 	if (number < 100)
 	{
 		printf(" ");
