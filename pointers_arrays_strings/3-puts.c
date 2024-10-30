6ad3bc275ade80580cbf1e721e09cb38d5255bcb
vitushan <vitushan@outlook.fr>
2024-10-30 19:49:59 +0000

    r

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,7 +3,6 @@
  /**
  * _puts - Affiche une chaîne de caractères sur la sortie standard.
  * @str: Pointeur vers la chaîne de caractères à afficher.
- *
  * Description: Cette fonction prend un pointeur vers une chaîne de 
  * caractères et affiche chaque caractère sur la sortie standard 
  * dans une boucle.
