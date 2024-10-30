49ebe3b4dbe295de7b926327708feab0a9b3a613
vitushan <vitushan@outlook.fr>
2024-10-30 10:08:53 +0000

    pointeur

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-
+#include "main.h"
+/**
+ * _strlen function  returns the lenght of a string.
+ * return : lenght of a string
+ * **/
 int _strlen(char *s)
 {
+	int longeurChaine = 0;	
 	
-
+	while (s[longeurChaine] != '\0')
+	{
+		longeurChaine++;
+	}
+	return longeurChaine;
 }
+
