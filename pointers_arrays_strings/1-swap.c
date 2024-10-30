de4331f4a963626c5f6cba8346f82789e16737f4
vitushan <vitushan@outlook.fr>
2024-10-30 08:28:55 +0000

    pointeur

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+/**
+ *  void swap_int(int *a, int *b)
+ *  @*a integer one
+ *  @*b integer two
+ *  temporaire is temp variable for reverse two int.
+ *  reverse the result in variable or pointer.
+ *  return: always  void.
+ *  **/
 void swap_int(int *a, int *b)
 {
- int temporaire = *a;
+	int temporaire = *a;
 	*a = *b;
 	*b = temporaire;
 }
-
