6293bfd4b0fcf6f233902bf3c6c83398ba9795ba
vitushan <vitushan@outlook.fr>
2024-10-30 19:36:46 +0000

    r

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ *void _puts est une fonction
+ *qui imprimes les chaines de caracteres
+ dans le pointeur str
+ *
+ * avec une boucle en incremantant i.
+ *return: void
+**/
 void _puts(char *str)
 {
 	int i = 0;
