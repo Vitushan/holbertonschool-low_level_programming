d9d0f29d94e4051a4e4ca0a28e03829d7c792e38
vitushan <vitushan@outlook.fr>
2024-10-30 10:16:31 +0000

    pointeur

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
 * _strlen - Returns the length of a string.
-* @s: The string whose length is to be measured. 
+* @s: The string whose length is to be measured.
 * Return: The length of the string.
 */
 int _strlen(char *s)
