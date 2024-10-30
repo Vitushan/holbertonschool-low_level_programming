09c8dbe398c6f0923c0337b20641c0150c95151c
vitushan <vitushan@outlook.fr>
2024-10-30 08:35:37 +0000

    pointeur

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,6 +4,7 @@
 *swap_int - Swaps the value of two integers.
 *@a: pointer to the first integer.
 *@*b pointer to the second integer.
+b is a and a is b.
 *return: always void.
 **/
 void swap_int(int *a, int *b)
