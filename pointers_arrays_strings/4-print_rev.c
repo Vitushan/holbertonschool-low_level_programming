85a03815319877483ab79bfba2382a7fc2cdeba8
vitushan <vitushan@outlook.fr>
2024-10-31 08:51:28 +0000

    r

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void print_rev
+ * @ caractere pointé sur *s 
+ * affiche la chaine de caractere a l'envers,
+ * suivis d'une nouvelle ligne.
+ * return: void.
+ * **/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
