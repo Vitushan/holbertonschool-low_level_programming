7fda98c970df04e544cbfaad37a76e2b2d61bf2b
vitushan <vitushan@outlook.fr>
2024-11-02 21:33:01 +0000

    arrays

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,14 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 	int started = 0;
+	
 	while (*s)
 	{
 		if (*s == '-' && !started)
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 	{
-		started = 1;
+	started = 1;
 	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && *s - '0' > INT_MAX % 10))
 	return (sign == 1) ? INT_MAX : INT_MIN;
 	result = result * 10 + (*s - '0');
@@ -26,5 +27,5 @@ int _atoi(char *s)
 	break;
 	s++;
 	}
-return (result * sign);
+	return (result * sign);
 }
