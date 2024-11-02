8b7b3bd19971a56a6637aa026e4d11336a9fc0c1
vitushan <vitushan@outlook.fr>
2024-11-02 21:18:18 +0000

    arrays

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copie une chaîne source dans une chaîne de destination
+ * @dest: Pointeur vers la chaîne de destination
+ * @src: Pointeur vers la chaîne source
+ *
+ * Return: Pointeur vers la chaîne de destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+    char *original_dest = dest;
+
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+
+    return original_dest;
+}
