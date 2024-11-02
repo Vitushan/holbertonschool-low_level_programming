52237de7d9f2ed6a452312de1eb8fba2345cac94
vitushan <vitushan@outlook.fr>
2024-11-02 21:12:44 +0000

    arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Imprime n éléments d'un tableau d'entiers
+ * @a: Le tableau d'entiers
+ * @n: Le nombre d'éléments à imprimer
+ *
+ * Description: Cette fonction imprime les n premiers éléments d'un tableau,
+ * séparés par une virgule et un espace, suivis d'une nouvelle ligne.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+	
+	if (i < n - 1)
+	{
+	printf(", ");
+	}
+}
+
+	printf("\n");
+}
