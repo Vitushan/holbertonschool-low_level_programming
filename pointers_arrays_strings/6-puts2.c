4ea9a13ddc0ad171371e85afcee31a654a34f819
vitushan <vitushan@outlook.fr>
2024-11-02 20:59:18 +0000

    arrays

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* puts2 - Imprime un caractère sur deux d'une chaîne
+* @str: La chaîne de caractères à traiter
+*
+* Description: Cette fonction parcourt la chaîne et imprime
+* les caractères aux positions paires (0, 2, 4, etc.),
+* suivis d'un retour à la ligne.
+*/
 void puts2(char *str)
 {
 	int i = 0;
