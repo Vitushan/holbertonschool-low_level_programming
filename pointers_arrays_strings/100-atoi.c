ce4a31f2f924fbb3bfbc1e35ce2ea31830b131d0
vitushan <vitushan@outlook.fr>
2024-11-02 21:25:59 +0000

    arrays

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -24,6 +24,7 @@ int _atoi(char *s)
 	{
 		digit = *s - '0';
 		if (result > (2147483647 - digit) / 10)
+			
 			return (sign == 1) ? 2147483647 : -2147483648;
 		result = result * 10 + digit;
 		s++;
