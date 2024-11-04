f15f58b0dc491262a5338a051ed42004166e9586
vitushan <vitushan@outlook.fr>
2024-11-04 13:32:09 +0000

    re

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * simple_print_buffer - prints buffer in decimal
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+* simple_print_buffer - prints buffer in decimal
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+* Return: Nothing.
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned i  = 0;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 	return (dest);
-
 }
