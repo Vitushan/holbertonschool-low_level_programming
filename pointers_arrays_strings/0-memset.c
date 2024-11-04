9b1998af554595c99a6e44c82732aaddf662a479
vitushan <vitushan@outlook.fr>
2024-11-04 09:52:11 +0000

    re

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+* char *_memset
+* La fonction _memset() remplit les n premiers octets de la zone mémoire 
+* pointée par s avec l'octet constant b
+*Renvoie un pointeur vers la zone mémoire s
+* @ *s, @b, @n.
+*return: retourne un  unsigned int.
+**/
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
