a0a668ed4aac1aab28eda6819c6807f250039f47
vitushan <vitushan@outlook.fr>
2024-11-04 19:11:05 +0000

    while

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+/**
+ * _strprbrk une fonction qui recherche dans une chaîne l'un des octets d'un ensemble. 
+ * prend en parametre @*s, @accept
+ * return: 0.
+ **/
 char *_strpbrk(char *s, char *accept)
 {
-    while (*s) {
-        char *a = accept;
-        while (*a) 
-        {
-            if (*s == *a)
-                return s;
-            a++;
-        }
-        s++;
-    }
-    return (0);
+	while (*s) {
+	char *a = accept;
+	while (*a) 
+	{
+		if (*s == *a)
+		return s;
+		a++;
+	}
+	s++;
+	}
+	return (0);
 }
