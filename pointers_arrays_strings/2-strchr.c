b50fe20a630a62f09c689dd663374237e183943e
vitushan <vitushan@outlook.fr>
2024-11-04 14:57:20 +0000

    re

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr - Renvoie un pointeur sur la première occurrence du caractère c dans la chaîne s
+* @s: La chaîne à parcourir
+* @c: Le caractère à trouver
+* Return: Un pointeur vers la première occurrence de c dans s, ou NULL si non trouvé
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		return (s);
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return (0);
+}
