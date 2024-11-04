9ca28f5f356c85ebb2ddca48670437359f74caf6
vitushan <vitushan@outlook.fr>
2024-11-04 15:41:59 +0000

    re

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strspn récupère la longueur d'une sous-chaîne de préfixes.
+* @*s @*accept 
+* Return: Renvoie le nombre d'octets dans le segment initial de s 
+* qui consiste uniquement en octets provenant de accept.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+
+
+}
