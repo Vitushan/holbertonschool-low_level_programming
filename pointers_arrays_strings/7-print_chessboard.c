237dac6f2b5f2f64747b98bfdbaa2760b1726bd5
Vitushan <vitushan@outlook.fr>
2024-11-05 14:29:53 +0100

    for

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Imprime un échiquier de 8x8
+ * @a: Tableau 2D de caractères représentant l'échiquier
+ */
+void print_chessboard(char (*a)[8])
+{
+    int i, j;
+    
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j <8; i++)
+        {
+            _putchar(a[i] [j]);
+        }
+        _putchar('\n');
+    }
+}
