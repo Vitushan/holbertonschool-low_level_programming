9ba01cadc3d0d4a10e1e65edca3e7b4ef6ae0e48
Vitushan <vitushan@outlook.fr>
2024-11-05 16:44:36 +0100

    re

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,21 @@
 #include "main.h"
 
+/**
+ * print_diagsums - Imprime la somme des deux diagonales d'une matrice carrée
+ * @a: Pointeur vers le premier élément de la matrice
+ * @size: Taille de la matrice (nombre de lignes ou colonnes)
+ */
 void print_diagsums(int *a, int size)
 {
-g;
+    int sum_main_diag = 0;
+    int sum_secondary_diag = 0;
+    int i;
 
+    for (i = 0; i < size; i++)
+    {
+        sum_main_diag += a[i * (size + 1)];        /*Diagonale principale */
+        sum_secondary_diag += a[(i + 1) * (size - 1)];  /*Diagonale secondaire */
+    }
+
+    printf("%d, %d\n", sum_main_diag, sum_secondary_diag);
 }
