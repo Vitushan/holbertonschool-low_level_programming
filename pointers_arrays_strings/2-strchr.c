7c4b5d0eb1e027b3c3106c37a398b9332f5217c5
Vitushan <vitushan@outlook.fr>
2024-11-05 09:49:47 +0100

    str

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
 * _strchr - Renvoie un pointeur sur la première occurrence du caractère c dans la chaîne s
 * @s: La chaîne à parcourir
 * @c: Le caractère à trouver
-* Return: Un pointeur vers la première occurrence de c dans s, ou NULL si non trouvé
+* Return: Un pointeur vers la première occurrence de c dans s, ou 0 si non trouvé
 */
 char *_strchr(char *s, char c)
 {
