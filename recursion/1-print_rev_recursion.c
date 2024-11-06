ade5dc1f7fc8ef204098b25a2857aa27a6744417
vitushan <vitushan@outlook.fr>
2024-11-06 15:06:41 +0000

    re

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_rev - Affiche une chaîne de caractères à l'envers,
+ * _print_rev_recursion - Affiche une chaîne de caractères à l'envers,
  * suivie d'une nouvelle ligne.
- * @s: Pointeur sur la chaîne de caractères à afficher en inverse.
- * Cette fonction prend une chaîne de caractères et l'affiche à l'envers,
- * Return: void.
+ * @s: Pointeur vers la chaîne de caractères à afficher en sens inverse.
+ *
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
+    if (*s == '\0') /* Condition de fin : Si on atteint la fin de la chaîne */
+    {
+        return; /* On sort de la récursion */
+    }
+    _print_rev_recursion(s + 1); /* Appel récursif avec le caractère suivant */
+    _putchar(*s); /* Imprime le caractère actuel après l'appel récursif */
 }
-
