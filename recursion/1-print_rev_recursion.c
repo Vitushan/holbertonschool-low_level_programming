0874af98ec976ba204a6e55ed17d0dbb245a9a3c
vitushan <vitushan@outlook.fr>
2024-11-06 15:04:26 +0000

    re

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,13 +9,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i;
-	
-	for (i = 0; s[i] != '\0'; i++)
-	
-		i--;
-	
-	for (; i >= 0; i--)
-		_putchar(s[i];
+	if (*s == '\0')
+	{
+		return;
+	}
 }
 
