9a03439c77b92ea5d2e6a3dcc7334bf670639b00
vitushan <vitushan@outlook.fr>
2024-11-06 14:40:25 +0000

    re

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
-* _puts_recursion - Imprime une chaîne de caractères 
-* suivie d'une nouvelle ligne.
-* @s: Pointeur vers la chaîne de caractères à imprimer
-* Return: Aucun (void)
-*/
+ * _puts_recursion - Imprime une chaîne de caractères
+ * suivie d'une nouvelle ligne.
+ * @s: Pointeur vers la chaîne de caractères à imprimer
+ * Return: Aucun (void)
+ */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')         /*Si on est à la fin de la chaîne*/
+	if (*s == '\0') /* Vérifie si on est à la fin de la chaîne */
 	{
-	_putchar('\n');     /*Imprime un retour à la ligne
-	return;              Fin de la récursion */
+		_putchar('\n'); /* Imprime un retour à la ligne */
+		return; /* Fin de la récursion */
 	}
-	_putchar(*s);           /*Imprime le caractère actuel*/
+	_putchar(*s); /* Imprime le caractère actuel */
+	_puts_recursion(s + 1); /* Appel récursif pour le caractère suivant */
 }
-
-
