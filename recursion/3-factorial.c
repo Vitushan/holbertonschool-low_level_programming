6cda7cca8e5472616330ead9a5936c8bf22b0012
Vitushan <vitushan@outlook.fr>
2024-11-07 23:02:56 +0100

    re

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,29 +1,16 @@
 #include "main.h"
 
-/** 
-* factorial - renvoie la factorielle d'un nombre donné.
-*@n sur le parametre n
-*Return: -1 indique une erreur si n est inferieur a zero.
-La factorielle de 0 est 1 .
-*/
+/**
+ * factorial - Renvoie la factorielle d'un nombre donné.
+ * @n: Nombre dont on veut calculer la factorielle.
+ *
+ * Return: La factorielle de n. Si n est négatif, retourne -1 pour indiquer une erreur.
+ */
 int factorial(int n)
 {
-    /* Déclaration de ma fonction factorialTehZebi*/ 
-int factorial = 0, nTehZebi = 0;
-    
-    if (nTehZebi < 0) {
-    return -1; //Erreur pour les nombres négatifs on renvoie -1
-  }
-
-  if (nTehZebi == 0) {
-    return 1; // La factorielle de 0 est 1 donc on renvoie 1
-  }
-
-  int resultatTehZebi = 1;
-  for (int i = 1; i <= nTehZebi; i++)//Tant que i <= nTehZebi on execute l'instruction
-  {
-    resultatTehZebi *= i; // ici on multiplie resultatTehZebi avec i
-  }
-
-  return resultatTehZebi;
-} 
+    if (n < 0)
+        return (-1);
+    if (n == 0)
+        return (1);
+    return (n * factorial(n - 1));
+}
