09d2ed951016d57ae5618cb9d43047a8db624120
Vitushan <vitushan@outlook.fr>
2024-11-07 22:49:47 +0100

    re

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,5 +8,22 @@ La factorielle de 0 est 1 .
 */
 int factorial(int n)
 {
+    /* Déclaration de ma fonction factorialTehZebi*/ 
+int factorial = 0, nTehZebi = 0;
     
+    if (nTehZebi < 0) {
+    return -1; //Erreur pour les nombres négatifs on renvoie -1
+  }
+
+  if (nTehZebi == 0) {
+    return 1; // La factorielle de 0 est 1 donc on renvoie 1
+  }
+
+  int resultatTehZebi = 1;
+  for (int i = 1; i <= nTehZebi; i++)//Tant que i <= nTehZebi on execute l'instruction
+  {
+    resultatTehZebi *= i; // ici on multiplie resultatTehZebi avec i
+  }
+
+  return resultatTehZebi;
 } 
