db712481ede9b41fdfe243b227abb069190f2882
Vitushan <vitushan@outlook.fr>
2024-11-07 22:46:33 +0100

    push

---
+++ recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** 
+* factorial - renvoie la factorielle d'un nombre donné.
+*@n sur le parametre n
+*Return: -1 indique une erreur si n est inferieur a zero.
+La factorielle de 0 est 1 .
+*/
+int factorial(int n)
+{
+    
+} 
