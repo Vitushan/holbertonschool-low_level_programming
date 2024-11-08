2f36afef5d321c0786b40f674fe925893e577ca7
Vitushan <vitushan@outlook.fr>
2024-11-08 20:16:19 +0100

    re

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -13,16 +13,16 @@ int _sqrt_helper(int n, int i)
         return (-1);
     if (i * i == n)     /* Si i^2 est égal à n, on a trouvé la racine carrée naturelle */
         return (i);
-    return (_sqrt_helper(n, i + 1)); /* Appel récursif avec i incrémenté */
-}/**
+        return (_sqrt_helper(n, i + 1)); /* Appel récursif avec i incrémenté */
+}
+/**
  * _sqrt_recursion - Renvoie la racine carrée naturelle d'un nombre.
  * @n: Le nombre dont on veut calculer la racine carrée.
- *
  * Return: La racine carrée naturelle de n ou -1 si elle n'existe pas.
  */
 int _sqrt_recursion(int n)
 {
     if (n < 0)
         return (-1); /* Un nombre négatif n'a pas de racine carrée naturelle */
-    return (_sqrt_helper(n, 1)); /* On commence à tester avec i = 1 */
+        return (_sqrt_helper(n, 1)); /* On commence à tester avec i = 1 */
 }
