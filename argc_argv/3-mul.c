fe90bc03f046017485dd080c77d7a91cf7b61b1b
Vitushan <vitushan@outlook.fr>
2024-11-09 12:15:46 +0100

    re

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int arfc, char *argv[])
+int main (int argc, char *argv[])
 {
-    if ("argc" != 3)
+    if (argc >= 3)
     {
     printf("Error\n");
     return (1);
