7f39bd8a10637bd6b908b20d216b36e4437d1ced
Vitushan <vitushan@outlook.fr>
2024-11-12 15:31:53 +0100

    malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,7 +9,7 @@ char *create_array(unsigned int size, char c)
         return (NULL);
 
         array =(char *)malloc(size * sizeof(char)); /*Allocation de memoire dynamique pr le tableau*/
-        if (array == NULL)   
+        if (array == NULL)
             return (NULL);/*Retourne NULL en cas d'echec d'allocation dynamique*/
 
             for (unsigned int i = 0; i < size; i++)
