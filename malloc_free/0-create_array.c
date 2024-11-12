753dc3c6cf900b7598b684580d56ce6b95117be8
Vitushan <vitushan@outlook.fr>
2024-11-12 15:46:45 +0100

    malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,23 +1,30 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * create_array - Crée un tableau de caractères et l'initialise
+ * @size: Taille du tableau à créer
+ * @c: Caractère pour initialiser chaque élément du tableau
+ * 
+ * Return: Pointeur vers le tableau initialisé, ou NULL en cas d'échec
+ */
 char *create_array(unsigned int size, char c)
 {
+	char *array; /* Déclaration du pointeur pour le tableau */
+	unsigned int i; /* Déclaration de l'index pour la boucle */
 
+	/* Vérifie si la taille est 0, retourne NULL dans ce cas */
+	if (size == 0)
+		return (NULL);
 
-        char *array;
-        unsigned int i;
+	/* Allocation dynamique de mémoire pour le tableau */
+	array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL); /* Retourne NULL en cas d'échec d'allocation */
 
-        if (size == 0)
-        return (NULL);
+	/* Remplit le tableau avec le caractère spécifié */
+	for (i = 0; i < size; i++)
+		array[i] = c;
 
-        array =(char *)malloc(size * sizeof(char)); /*Allocation de memoire dynamique pr le tableau*/
-        if (array == NULL)
-            return (NULL);/*Retourne NULL en cas d'echec d'allocation dynamique*/
-            
-              for (i = 0; i < size; i++)
-            {
-                array[i] = c;
-            }
-            return (array);
+	return (array); /* Retourne le pointeur vers le tableau créé */
 }
