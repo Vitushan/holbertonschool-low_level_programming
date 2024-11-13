2264dc0379f98267cadc2a711bd170bdbf708f26
Vitushan <vitushan@outlook.fr>
2024-11-13 15:09:35 +0100

    free

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,11 @@ void *malloc_checked(unsigned int b)
     {
         exit (98);
     }
-
-    return (ptr);
+return (ptr);
 }
+
+/*Ecrivez une fonction qui alloue de la mémoire en utilisant malloc.
+Prototype : void *malloc_checked(unsigned int b) ;
+Renvoie un pointeur sur la mémoire allouée
+si malloc échoue, la fonction malloc_checked doit provoquer la fin normale du processus avec un statut de 98*/
 
