36c4582ef95537a801176bad6b06db2aa8231f47
Vitushan <vitushan@outlook.fr>
2024-11-14 15:28:49 +0100

    malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         concat[i] = s1[i];
     for (i = 0; i < n; i++)
         concat[len1 + i] = s2[i];
-        concat[len 1 + n] = '\0';
+        concat[len1 + n] = '\0';
 
         return (concat);
 
