37a03114fb39af849d2bb251edd232c348637e31
Vitushan <vitushan@outlook.fr>
2024-11-15 20:28:50 +0100

    re

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,22 +9,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int total_size;
-    char *ptr;
-    unsigned int i;
+	unsigned int total_size;
+	char *ptr;
+	unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    total_size = nmemb * size;
+	total_size = nmemb * size;
 
-    ptr = malloc(total_size);
+	ptr = malloc(total_size);
 
-    if (ptr == NULL)
-        return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
-    for (i = 0; i < total_size; i++)
-        ptr[i] = 0;
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
 
-    return (ptr);
+	return (ptr);
 }
