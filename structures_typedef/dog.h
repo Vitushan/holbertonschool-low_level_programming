7eb9d116fc9bcc35b6876c0b090ee88cc078cc26
Vitushan <vitushan@outlook.fr>
2024-11-15 20:34:08 +0100

    re

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Représente un chien avec ses informations de base
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
+ *
+ * Description: Structure contenant les informations essentielles d'un chien
+ */
 struct dog
 {
 char *name;
