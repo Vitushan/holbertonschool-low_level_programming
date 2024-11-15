e6165aaf37cb236b398ffa2327f6024f7d342f57
Vitushan <vitushan@outlook.fr>
2024-11-15 22:44:22 +0100

    re

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,40 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strlen - Calcule la longueur d'une chaîne
+ * @s: Chaîne à mesurer
+ *
+ * Return: Longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - Copie une chaîne
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: Pointeur vers dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * new_dog - Crée un nouveau chien
@@ -13,30 +47,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_copy, *owner_copy;
+	int name_len, owner_len;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_copy = strdup(name);
-	if (name_copy == NULL)
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	owner_copy = strdup(owner);
-	if (owner_copy == NULL)
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (new_dog->owner == NULL)
 	{
-		free(name_copy);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = name_copy;
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->owner = owner_copy;
 
 	return (new_dog);
 }
