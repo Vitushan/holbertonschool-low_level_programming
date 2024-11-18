bc697db96925eb519facd85349db75c02a816628
Vitushan <vitushan@outlook.fr>
2024-11-18 16:03:52 +0100

    re

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -8,4 +8,17 @@ int op_mul(int a, int b);
 int op_sub(int a, int b);
 int op_add(int a, int b);
 
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
 #endif
