8047faf5246f21a69ba8f40bc06f22e500ca9a36
Vitushan <vitushan@outlook.fr>
2024-11-18 11:09:47 +0100

    re

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
+* print_name - Appelle une fonction pour imprimer un nom d'une manière spécifique.
+* @name : pointeur vers le nom à imprimer.
+* @f : pointeur vers une fonction qui prend un char * et renvoie void.
+*
+* Return : rien.
+*/
 void print_name(char *name, void (*f)(char *))
 {
-
+	if (name != NULL && f != NULL)   /*Vérification des pointeurs*/
+		f(name);  /*Appelle la fonction passée en argument*/
 }
