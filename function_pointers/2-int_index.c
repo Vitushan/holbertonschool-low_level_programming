801cbc793890b6c35be08fc69ce41f4a5e57eafc
Vitushan <vitushan@outlook.fr>
2024-11-18 16:01:47 +0100

    re

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,13 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp && size > 0)
-	{
-		for (i = 0; i < size; i++)
+	if (size <= 0)
+		return (-1);
+
+		if (array && cmp)
 		{
-			if (cmp(array[i]))
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
 				return (i);
+			}
 		}
-	}
 	return (-1);
 }
