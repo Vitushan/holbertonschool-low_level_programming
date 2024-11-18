5114c3fc53237ecfb8555463349138ff3bba1106
Vitushan <vitushan@outlook.fr>
2024-11-18 14:37:48 +0100

    re

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** array_iterator - execute une fonction donnée
- * sur chaque element d'un tableau.
- * @array: pointeur vers le tableau d'entiers.
- * @size: Taille du tableau
- * @action: pointeur ver la fonction a executer pour chaque elements.
-
+/**
+* array_iterator - execute une fonction donnée
+* sur chaque element d'un tableau.
+* @array: pointeur vers le tableau d'entiers.
+* @size: Taille du tableau
+* @action: pointeur ver la fonction a executer pour chaque elements.
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL && action != NULL)
-		{
+		if (array != NULL && action != NULL)
 			for (i = 0; i < size; i++)
-			{
 				action(array[i]);
-			}
-		}
 }
