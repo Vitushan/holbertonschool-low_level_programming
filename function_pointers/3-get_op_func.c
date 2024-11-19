a98bce6d1a9a106b5a1e6c1fa1d77c5383b66cf4
Vitushan <vitushan@outlook.fr>
2024-11-19 14:07:17 +0100

    re

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,21 +5,21 @@
 /**
  *  get_op_func - selectionne les bonne operations de calcul
  * @s: operateur choisit par l'utilisateur.
- * return: un pointeur de fonction qui correspond
+ * Return: un pointeur de fonction qui correspond
  *  a l'operateur donner en parametre s.
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-	
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
 	while (ops[i].op != NULL)
 	{
 		if (*ops[i].op == *s && strlen(s) == 1)
