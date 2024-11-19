2b84e53c64f955fedf10c6d094117d63425e0fcb
Vitushan <vitushan@outlook.fr>
2024-11-19 20:27:32 +0100

    re

--- function_pointers/variadic_functions/0-sum_them_all.c
+++ function_pointers/variadic_functions/0-sum_them_all.c
@@ -7,24 +7,23 @@
  * de tout ses parametres
  * @n: nombre
  * Return: result
- *
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	int result = 0;
 	unsigned int i = 0;
-	va_list ap;
+	va_list app;
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
+	va_start(app, n);
 	while (i < n)
 	{
-		result += va_arg(ap, int);
+		result += va_arg(app, int);
 		i++;
 	}
-	va_end(ap);
+	va_end(app);
 
 	return (result);
 }
