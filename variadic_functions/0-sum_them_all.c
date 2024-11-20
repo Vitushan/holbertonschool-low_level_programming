f6e7b1445cfedb6e5d5322fb97601a078208ea81
Vitushan <vitushan@outlook.fr>
2024-11-20 11:14:36 +0100

    push

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,19 +10,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int result = 0;
+	int result = 0; /*stock la somme des nombres (resultat)*/
 	unsigned int i;
-	va_list ptr;
+	va_list ptr; /*permet de gerer les arguments variadiques*/
 
 	if (n == 0)
 	return (0);
 
-		va_start(ptr, n);
+		va_start(ptr, n); /*initialise ptr pr qu'il pointe vers les arguments apres n*/
 
-			for (i = 0; i < n; i++)
-			result += va_arg(ptr, int);
+			for (i = 0; i < n; i++) /*boucle pour parcourir tout les arguments*/
+			result += va_arg(ptr, int); /* va_arg(ptr, int) ajoute chaque argument a la somme (resultat) dans result*/
 
-				va_end(ptr);
+				va_end(ptr); /*fin de lecture des arguments, lorsque les arguments sont traités on libere ptr (Libère la mémoire de la liste) */
 
 					return (result);
 }
--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
