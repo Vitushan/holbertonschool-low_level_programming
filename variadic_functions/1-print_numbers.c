0fda24f2983601d39117f474c4a903c1578db2be
Vitushan <vitushan@outlook.fr>
2024-11-20 09:39:09 +0100

    push

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	
+}
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+void print_all(const char * const format, ...)
+{
+	
+}
