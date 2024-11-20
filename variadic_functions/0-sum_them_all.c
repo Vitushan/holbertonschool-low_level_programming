099a7d0ba95a136155b2d04fc8581ffeca41e18a
Vitushan <vitushan@outlook.fr>
2024-11-20 10:15:48 +0100

    push

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - fonction qui renvoie la somme
+ * de tout ses parametres
+ * @n: nombre
+ * Return: result
+ */
 int sum_them_all(const unsigned int n, ...)
 {
+	int result = 0;
+	unsigned int i; 
+	va_list ptr;
+
+	if (n == 0)
+	return (0);
 	
+		va_start(ptr, n);
+	
+			for(i = 0; i < n; i++)
+			result += va_arg(ptr, int);
+	
+				va_end(ptr);
+
+					return (result);
 }
