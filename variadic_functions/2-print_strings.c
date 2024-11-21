8ffab8927190331396b23e89907e28a8542278de
Vitushan <vitushan@outlook.fr>
2024-11-21 11:09:46 +0100

    push

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,13 +18,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(list, char*));
-		if (separator != NULL && i != n - 1)
-		printf("%s", separator);
+		if (va_arg(list, char*) == NULL)
+			printf("nil");
+		else
+			printf("%s", va_arg(list, char*));
+		if (separator != NULL && i != n - 1) /*!= different de / ou n'est pas */
+			printf("%s", separator);
 	}
-	if (list == NULL)
-	{
-		printf("nil");
-	}
+
 		printf("\n");
+
+			va_end(list);
 }
