4661acba52e1502ed73a486ba951bd75cec32583
Vitushan <vitushan@outlook.fr>
2024-12-02 16:24:51 +0100

    re

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,4 +6,28 @@
 size_t print_list(const list_t *h)
 {
 	
+	size_t count = 0;
+	
+	while (h != NULL)
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%lu] %s\n", h->len, h->str);
+	}
+	count++;
+	h = h ->next;
+
+		return (count);
+
 }
+
+/*Écrire une fonction qui imprime tous les éléments d'une liste list_t.
+
+Prototype : size_t print_list(const list_t *h) ;
+Retour : le nombre de noeuds
+Format : voir l'exemple
+Si str est NULL, print [0] (nil)
+Vous pouvez utiliser printf*/
