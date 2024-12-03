646f64cc4beb97d0fc99461026ce6addec696045
Vitushan <vitushan@outlook.fr>
2024-12-03 10:35:31 +0100

    final

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,6 +6,7 @@
 /**
  * print_list - Imprime tous les éléments d'une liste de type list_t.
  * @h: Pointeur vers le premier nœud de la liste.
+ * @size_t: imprime le nombre total de caractere.
  * Return: Le nombre total de caractères dans toutes les chaînes.
  */
 size_t print_list(const list_t *h)
