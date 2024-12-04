0b99fd7489f1779872837aaa4a60eb227d5c0abe
Vitushan <vitushan@outlook.fr>
2024-12-04 15:07:02 +0100

    re

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,10 +3,38 @@
 #include <stdio.h>
 #include <string.h>
 
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	
+	if (str == NULL)
+	return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	*head = new_node;
+	new_node->_strlen(str);
 
 	return (new_node);
 }
