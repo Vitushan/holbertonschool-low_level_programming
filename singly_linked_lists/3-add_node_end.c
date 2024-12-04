93696328d29166c19c59591cbdae761665328752
Vitushan <vitushan@outlook.fr>
2024-12-04 16:09:16 +0100

    re

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,8 @@ unsigned int _strlen(const char *str)
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	
+	list_t *last_node;
+
 	if (str == NULL)
 	return (NULL);
 
@@ -34,9 +35,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
-	*head = new_node;
 
-	return (new_node);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	last_node = *head;
+
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+
+		last_node->next = new_node;
+
+	return (new_node);	
 }
 
 /**
