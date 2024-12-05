da21c633b9e07583391b65d1cb67db27847b93c5
Vitushan <vitushan@outlook.fr>
2024-12-05 15:05:02 +0100

    chaine

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,3 +2,19 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+/**
+Écrivez une fonction qui renvoie le nombre d’éléments dans une liste dlistint_t chaînée.
+ */
