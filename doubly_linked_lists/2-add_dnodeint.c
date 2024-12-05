0007b13aed3b6171baadd7f2b0f3a36e0614db07
Vitushan <vitushan@outlook.fr>
2024-12-05 16:45:32 +0100

    chaine

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,7 +5,6 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *last_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 
@@ -13,22 +12,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 		new_node->n = n;
-		new_node->next = NULL;
 		new_node->prev = NULL;
+		new_node->next = *head;
 
-		if (*head == NULL)
-			{
-				*head = new_node;
-				return (new_node);
-			}
+		if (*head != NULL)
+			(*head)->prev = new_node;
 
-		last_node = *head;
-
-		while (last_node->next != NULL)
-		last_node = last_node->next;
-
-		last_node->next = new_node;
-		new_node->prev = last_node;
-
+		*head = new_node;
 	return (new_node);
 }
