889d21fea8d400238fff075e318671677c02814d
Vitushan <vitushan@outlook.fr>
2024-12-05 15:22:55 +0100

    chaine

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint_len - est une fonction qui retourne le nombre d'élement.
- * @h: est un pointeur qui pointe ver le premier noeud
- * Return: le nombre d'elements.
- */
+* print_dlistint_len - est une fonction qui retourne le nombre d'élement.
+* @h: est un pointeur qui pointe ver le premier noeud
+* Return: le nombre d'elements.
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
