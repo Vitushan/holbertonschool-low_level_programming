aed398ff7135d1ae169fbddbd71ee14bb575bd0d
Vitushan <vitushan@outlook.fr>
2024-12-05 19:52:07 +0100

    chaine

---
+++ doubly_linked_lists/teste.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - ajoute un nouveau noeud au debut de la liste
+ * dlistint_t
+ * @head:double pointeur vers la tete de la liste
+ * @n:valeur a ajouter dans le nouvel noeud
+ * Return: Adresse du nouvele éléments ou null en cas d'echec.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	
+}
