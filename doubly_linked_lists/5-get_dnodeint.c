42631f2b383278e52e7330f8f4438c7b4d89e57a
Vitushan <vitushan@outlook.fr>
2024-12-06 10:28:25 +0100

    re

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- *get_dnodeint_at_index - Renvoie le nième nœud d'une liste doubly linked list.
+ * get_dnodeint_at_index -renvoie le nième nœudd'une liste double.
  *@head: Pointeur vers le premier nœud de la liste.
  *@index: Index du nœud, à partir de 0.
 * Return: Pointeur vers le nième nœud, ou NULL si le nœud n'existe pas.
