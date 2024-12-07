e075abd573ff84a48c294714f440f2fe8ddc367d
Vitushan <vitushan@outlook.fr>
2024-12-07 18:41:42 +0100

    re

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,12 +4,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return -1;
+	int fd;
+	ssize_t len, bytes_written;
+
+	if (filename == NULL)
+        return (-1);
 
     int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd == -1)
-        return -1;
+        return (-1);
 
     if (text_content != NULL)
     {
@@ -20,10 +23,10 @@ int create_file(const char *filename, char *text_content)
         if (bytes_written == -1 || bytes_written != len)
         {
             close(fd);
-            return -1;
+            return (-1);
         }
     }
 
     close(fd);
-    return 1;
+    return (1);
 }
