b89ebd985aa49171231103fa55610abc060ae509
Vitushan <vitushan@outlook.fr>
2024-12-07 18:38:30 +0100

    re

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,19 +1,21 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
+#include <sys/stat.h>
 
 int create_file(const char *filename, char *text_content)
 {
     if (filename == NULL)
         return -1;
 
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd == -1)
         return -1;
 
     if (text_content != NULL)
     {
-        ssize_t len = strlen(text_content);
+        ssize_t len = 0;
+        while (text_content[len])
+            len++;
         ssize_t bytes_written = write(fd, text_content, len);
         if (bytes_written == -1 || bytes_written != len)
         {
@@ -23,5 +25,5 @@ int create_file(const char *filename, char *text_content)
     }
 
     close(fd);
-    return (1);
+    return 1;
 }
